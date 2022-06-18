76c52d040d41366a9acd863ed1c66465c4e6bb45
MatanSaban <105128031+MatanSaban@users.noreply.github.com>
2022-06-19 02:15:16 +0300

    app fix

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import {AiFillCheckCircle} from 'react-icons/ai'
 export const ThemeContext = createContext(null);
 
 function App() {
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState("dark");
 
     const toggleTheme = () => {
         setTheme((curr) => (curr === "light" ? "dark" : "light"));
