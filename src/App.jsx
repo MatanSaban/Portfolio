3f8cf6e57dfb2e3e94f143339d4b5d4e9fb9bb96
MatanSaban <105128031+MatanSaban@users.noreply.github.com>
2022-06-18 23:35:37 +0300

    light and dark mode fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,49 @@ import Services from "./Components/services/Services.jsx";
 import Testimonials from "./Components/testimonials/Testimonials.jsx";
 import { createContext, useState } from "react";
 import ReactSwitch from "react-switch";
+import {AiOutlineCheckCircle} from 'react-icons/ai'
+import {AiFillCheckCircle} from 'react-icons/ai'
 
 export const ThemeContext = createContext(null);
 
 function App() {
+    const [theme, setTheme] = useState("light");
 
-  const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+        setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    };
 
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
-  }
-
-  return (
-    <ThemeContext.Provider value={{theme, toggleTheme}}>
-      <div className="App" id={theme}>
-        <>
-          <Header />
-          <Nav themeMode={<ReactSwitch className="themeModeSwitcher" onChange={toggleTheme} checked={theme === 'dark'}/>}/>
-          <About />
-          <Experience />
-          <Services />
-          <Portfolio />
-          <Testimonials />
-          <Contact />
-          <Footer />
-        </>
-      </div>
-    </ThemeContext.Provider>
-  );
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            <div className="App" id={theme}>
+                <>
+                    <Header />
+                    <Nav
+                        themeMode={
+                            <ReactSwitch
+                                className="themeModeSwitcher"
+                                onChange={toggleTheme}
+                                checked={theme === "dark"}
+                                uncheckedIcon={
+                                  <AiFillCheckCircle style={{color:'var(--color-primary-lmode)', width:'100%', height:'100%'}}/>
+                                }
+                                checkedIcon={
+                                  <AiFillCheckCircle style={{color:'var(--color-bg)', width:'100%', height:'100%'}}/>
+                                }
+                            />
+                        }
+                    />
+                    <About />
+                    <Experience />
+                    <Services />
+                    <Portfolio />
+                    <Testimonials />
+                    <Contact />
+                    <Footer />
+                </>
+            </div>
+        </ThemeContext.Provider>
+    );
 }
 
 export default App;
