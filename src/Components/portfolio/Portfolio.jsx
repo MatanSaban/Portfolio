5c07d7b53610b0b8b2fc97248e55861bb58fe88b
MatanSaban <105128031+MatanSaban@users.noreply.github.com>
2022-06-19 21:31:34 +0300

    change

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,22 +1,81 @@
-import './portfolio.css'
-import IMG from '../../assets/profileimage.jpg'
-import PortfolioItem from './PortfolioItem';
+import "./portfolio.css";
+import IMG from "../../assets/profileimage.jpg";
+import PortfolioItem from "./PortfolioItem";
+import { useRef } from "react";
+import { useInViewport } from "react-in-viewport";
+// import Swiper core and required modules
+import { Navigation, Pagination, A11y } from "swiper";
 
-const Portfolio = () => {
-    return ( 
-        <section id='portfolio'>
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const Portfolio = (props) => {
+    const myRef = useRef();
+    const { inViewport } = useInViewport(myRef);
+
+    return (
+        <section id="portfolio" ref={myRef}>
+            {inViewport && props.checkNav("portfolio")}
             <h5>My Recent Work</h5>
             <h2>Portfolio</h2>
             <div className="container portfolio__container">
-                <PortfolioItem imageSrc={IMG} title={'This is a portfolio item title'} github={'https://github.com/matansaban'} liveDemo={'https://github.com'}/>
-                <PortfolioItem imageSrc={IMG} title={'This is a portfolio item title'} github={'https://github.com/matansaban'} liveDemo={'https://github.com'}/>
-                <PortfolioItem imageSrc={IMG} title={'This is a portfolio item title'} github={'https://github.com/matansaban'} liveDemo={'https://github.com'}/>
-                <PortfolioItem imageSrc={IMG} title={'This is a portfolio item title'} github={'https://github.com/matansaban'} liveDemo={'https://github.com'}/>
-                <PortfolioItem imageSrc={IMG} title={'This is a portfolio item title'} github={'https://github.com/matansaban'} liveDemo={'https://github.com'}/>
-                <PortfolioItem imageSrc={IMG} title={'This is a portfolio item title'} github={'https://github.com/matansaban'} liveDemo={'https://github.com'}/>
+                <Swiper
+                    modules={[Navigation, Pagination, A11y]}
+                    spaceBetween={40}
+                    slidesPerView={1}
+                    pagination={{ clickable: true }}
+                    navigation={true}
+                    loop={true}
+                >
+                    <SwiperSlide>
+                        <PortfolioItem
+                            imageSrc={IMG}
+                            title={"This is a portfolio item title"}
+                            github={"https://github.com/matansaban"}
+                            liveDemo={"https://github.com"}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <PortfolioItem
+                            imageSrc={IMG}
+                            title={"This is a portfolio item title"}
+                            github={"https://github.com/matansaban"}
+                            liveDemo={"https://github.com"}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <PortfolioItem
+                            imageSrc={IMG}
+                            title={"This is a portfolio item title"}
+                            github={"https://github.com/matansaban"}
+                            liveDemo={"https://github.com"}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <PortfolioItem
+                            imageSrc={IMG}
+                            title={"This is a portfolio item title"}
+                            github={"https://github.com/matansaban"}
+                            liveDemo={"https://github.com"}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <PortfolioItem
+                            imageSrc={IMG}
+                            title={"This is a portfolio item title"}
+                            github={"https://github.com/matansaban"}
+                            liveDemo={"https://github.com"}
+                        />
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </section>
-     );
-}
- 
+    );
+};
+
 export default Portfolio;
--- src/Components/portfolio/PortfolioItem.jsx
+++ src/Components/portfolio/PortfolioItem.jsx
@@ -1,24 +1,37 @@
 const PortfolioItem = (props) => {
     return (
-        <article className="portfolio__item">
-            <div className="portfolio__item-image">
-                <img src={props.imageSrc} alt="" />
+        <>
+            <article
+                className="portfolio__item"
+                style={{
+                    background: `url(${props.imageSrc})`,
+                    backgroundPosition: "center",
+                    height: "500px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent:'center',
+                    alignItems:'center',
+                    borderRadius:'20px'
+                }}
+            >
+            </article>
+            <div className="coverLayer" style={{background:'#00000040', height:'500px', width:'100%', position:'absolute', left:'0', top:'0', zIndex:'1', display:'flex', flexDirection:'column' ,alignItems:'center', justifyContent:'center' }}>
+                <h3>{props.title}</h3>
+                    <div className="portfolio__item-cta">
+                        <a href={props.github} className="btn">
+                            GitHub
+                        </a>
+                        <a
+                            href={props.liveDemo}
+                            className="btn btn-primary"
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            Live Demo
+                        </a>
+                    </div>
             </div>
-            <h3>{props.title}</h3>
-            <div className="portfolio__item-cta">
-                <a href={props.github} className="btn">
-                    GitHub
-                </a>
-                <a
-                    href={props.liveDemo}
-                    className="btn btn-primary"
-                    rel="noreferrer"
-                    target="_blank"
-                >
-                    Live Demo
-                </a>
-            </div>
-        </article>
+        </>
     );
 };
 
